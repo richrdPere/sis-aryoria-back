984f696e631750ea2f0b0f866fd4d3628e1529cc
richrdPere <richrdpere1234@gmail.com>
2026-08-12 18:41:34 -0500

    fix: usar tabla personas en modelo Persona

--- src/database/models/personas/persona.model.js
+++ src/database/models/personas/persona.model.js
@@ -1,73 +1,84 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../../../config/database");
 
-const Persona = sequelize.define("Persona", {
-  id_persona: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+const Persona = sequelize.define(
+  "Persona",
+  {
+    id_persona: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-  nombres: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
+    nombres: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
 
-  apellidos: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
+    apellidos: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
 
-  email: {
-    type: DataTypes.STRING(150),
-    validate: {
-      isEmail: true
-    }
-  },
+    email: {
+      type: DataTypes.STRING(150),
+      validate: {
+        isEmail: true,
+      },
+    },
 
-  tipo_documento: {
-    type: DataTypes.ENUM(
-      "DNI",
-      "RUC",
-      "CE",
-      "PASAPORTE"
-    ),
-    defaultValue: "DNI",
-  },
+    tipo_documento: {
+      type: DataTypes.ENUM(
+        "DNI",
+        "RUC",
+        "CE",
+        "PASAPORTE",
+      ),
+      defaultValue: "DNI",
+    },
 
-  numero_documento: {
-    type: DataTypes.STRING(20),
-    unique: true,
-  },
+    numero_documento: {
+      type: DataTypes.STRING(20),
+      unique: true,
+    },
 
-  fecha_nacimiento: {
-    type: DataTypes.DATEONLY,
-  },
+    fecha_nacimiento: {
+      type: DataTypes.DATEONLY,
+    },
 
-  celular: {
-    type: DataTypes.STRING(20),
-  },
+    celular: {
+      type: DataTypes.STRING(20),
+    },
 
-  direccion: {
-    type: DataTypes.STRING(255),
-  },
+    direccion: {
+      type: DataTypes.STRING(255),
+    },
 
-  foto_url: {
-    type: DataTypes.STRING,
-  },
+    foto_url: {
+      type: DataTypes.STRING,
+    },
 
-  genero: {
-    type: DataTypes.ENUM(
-      "M",
-      "F",
-      "OTRO"
-    ),
-  },
+    genero: {
+      type: DataTypes.ENUM(
+        "M",
+        "F",
+        "OTRO",
+      ),
+    },
 
-  estado: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
+    estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   },
-});
+  {
+    tableName: "personas",
+
+    timestamps: true,
+
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+  },
+);
 
 module.exports = Persona;
